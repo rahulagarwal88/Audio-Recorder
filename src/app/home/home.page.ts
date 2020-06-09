ec4809a81d285ddfdd6b2d357211cc8568166e41
Rahul Agarwal <rahul.agarwal88@gmail.com>
2020-06-09 12:34:13 +0530

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+import { NavController, Platform } from '@ionic/angular';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { File } from '@ionic-native/file/ngx';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  recording: boolean = false;
+  filePath: string;
+  fileName: string;
+  audio: MediaObject;
+  audioList: any[] = [];
+
+  constructor(public navCtrl: NavController,
+    private media: Media,
+    private file: File,
+    public platform: Platform) {}
+
+    getAudioList() {
+      if(localStorage.getItem("audiolist")) {
+        this.audioList = JSON.parse(localStorage.getItem("audiolist"));
+        console.log(this.audioList);
+      }
+    }
+
+    ionViewWillEnter() {
+      this.getAudioList();
+    }
+
+    startRecord() {
+      if (this.platform.is('ios')) {
+        this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.m4a';
+        this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + this.fileName;
+        this.audio = this.media.create(this.filePath);
+      } else if (this.platform.is('android')) {
+        this.fileName = 'record'+new Date().getDate()+new Date().getMonth()+new Date().getFullYear()+new Date().getHours()+new Date().getMinutes()+new Date().getSeconds()+'.m4a';
+        this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + this.fileName;
+        this.audio = this.media.create(this.filePath);
+      }
+      this.audio.startRecord();
+      this.recording = true;
+    }
+
+    stopRecord() {
+      this.audio.stopRecord();
+      let data = { filename: this.fileName };
+      this.audioList.push(data);
+      localStorage.setItem("audiolist", JSON.stringify(this.audioList));
+      this.recording = false;
+      alert(JSON.stringify(this.audioList));
+      this.getAudioList();
+    }
+
+    playAudio(file,idx) {
+      if (this.platform.is('ios')) {
+        this.filePath = this.file.documentsDirectory.replace(/file:\/\//g, '') + file;
+        this.audio = this.media.create(this.filePath);
+      } else if (this.platform.is('android')) {
+        this.filePath = this.file.externalDataDirectory.replace(/file:\/\//g, '') + file;
+        this.audio = this.media.create(this.filePath);
+      }
+      this.audio.play();
+      this.audio.setVolume(0.8);
+    }
+
+}
